3da982ca151b4566d5178994c26dd75c68a3e745
gyulii <72323941+gyulii@users.noreply.github.com>
2021-06-14 11:37:41 +0200

    Lefordul a regi kod megint

--- source/main.c
+++ source/main.c
@@ -29,6 +29,181 @@ int main(void)
 {
     InitSysCtrl();
 
+    EALLOW;
+    #if (CPU_FRQ_150MHZ)     // Default - 150 MHz SYSCLKOUT
+        //
+        // HSPCLK = SYSCLKOUT/2*ADC_MODCLK2 = 150/(2*3)   = 25.0 MHz
+        //
+        #define ADC_MODCLK 0x3
+    #endif
+    #if (CPU_FRQ_100MHZ)
+        //
+        // HSPCLK = SYSCLKOUT/2*ADC_MODCLK2 = 100/(2*2)   = 25.0 MHz
+        //
+        #define ADC_MODCLK 0x2
+    #endif
+    EDIS;
+    //
+    // Define ADCCLK clock frequency ( less than or equal to 25 MHz )
+    // Assuming InitSysCtrl() has set SYSCLKOUT to 150 MHz
+    //
+    EALLOW;
+    SysCtrlRegs.HISPCP.all = ADC_MODCLK;
+    EDIS;
+
+    //
+    // Step 2. Initialize GPIO:
+    // This example function is found in the DSP2833x_Gpio.c file and
+    // illustrates how to set the GPIO to it's default state.
+    //
+    // InitGpio();  // Skipped for this example
+
+    //
+    // Step 3. Clear all interrupts and initialize PIE vector table:
+    // Disable CPU interrupts
+    //
+    DINT;
+
+    //
+    // Initialize the PIE control registers to their default state.
+    // The default state is all PIE interrupts disabled and flags
+    // are cleared.
+    // This function is found in the DSP2833x_PieCtrl.c file.
+    //
+    InitPieCtrl();
+
+    //
+    // Disable CPU interrupts and clear all CPU interrupt flags:
+    //
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    //
+    // Initialize the PIE vector table with pointers to the shell Interrupt
+    // Service Routines (ISR).
+    // This will populate the entire table, even if the interrupt
+    // is not used in this example.  This is useful for debug purposes.
+    // The shell ISR routines are found in DSP2833x_DefaultIsr.c.
+    // This function is found in DSP2833x_PieVect.c.
+    //
+    InitPieVectTable();
+
+    //
+    // Interrupts that are used in this example are re-mapped to
+    // ISR functions found within this file.
+    //
+    EALLOW;  // This is needed to write to EALLOW protected register
+    PieVectTable.ADCINT = &adc_isr;
+    EDIS;    // This is needed to disable write to EALLOW protected registers
+
+    //
+    // Step 4. Initialize all the Device Peripherals:
+    // This function is found in DSP2833x_InitPeripherals.c
+    //
+    // InitPeripherals(); // Not required for this example
+    InitAdc();  // For this example, init the ADC
+
+    // Step 5. User specific code, enable interrupts:
+     //
+
+     //
+     // Enable ADCINT in PIE
+     //
+     PieCtrlRegs.PIEIER1.bit.INTx6 = 1;
+     IER |= M_INT1;      // Enable CPU Interrupt 1
+     EINT;               // Enable Global interrupt INTM
+     ERTM;               // Enable Global realtime interrupt DBGM
+
+     LoopCount = 0;
+     ConversionCount = 0;
+
+     //
+     // Configure ADC
+
+
+     //
+
+     AdcRegs.ADCTRL3.bit.SMODE_SEL = 0x1;
+
+
+     AdcRegs.ADCMAXCONV.all = 0x0001;       // Setup 2 conv's on SEQ1
+     AdcRegs.ADCCHSELSEQ1.bit.CONV00 = 0x6; // Setup ADCINA3 as 1st SEQ1 conv.
+
+     //
+     // Enable SOCA from ePWM to start SEQ1
+     //
+     //
+     AdcRegs.ADCTRL2.bit.EPWM_SOCA_SEQ1 = 1;
+
+     AdcRegs.ADCTRL2.bit.INT_ENA_SEQ1 = 1;  // Enable SEQ1 interrupt (every EOS)
+
+     //
+     //
+     // Assumes ePWM1 clock is already enabled in InitSysCtrl();
+     //
+     EPwm1Regs.ETSEL.bit.SOCAEN = 1;     // Enable SOC on A group
+     EPwm1Regs.ETSEL.bit.SOCASEL = 4;    // Select SOC from from CPMA on upcount
+     EPwm1Regs.ETPS.bit.SOCAPRD = 1;     // Generate pulse on 1st event
+     EPwm1Regs.CMPA.half.CMPA = 0x0080;  // Set compare A value
+     EPwm1Regs.TBPRD = 0xFFFF;           // Set period for ePWM1
+     EPwm1Regs.TBCTL.bit.CTRMODE = 0;    // count up and start
+
+     //
+     // Wait for ADC interrupt
+     //
+
+
+     for(;;)
+     {
+         LoopCount++;
+     }
 
 
 }
+
+
+
+
+__interrupt void
+adc_isr(void)
+{
+    /*       MIN megtalalas        */
+
+     min_value_actual = (AdcRegs.ADCRESULT0 >> 4) -1535;
+     if(min_value_actual < min_value_result) min_value_result = min_value_actual;
+     min_value_last = min_value_actual;
+
+
+    /* MIN megtalals                */
+
+    Voltage1[ConversionCount] = (AdcRegs.ADCRESULT0 >> 4) - 1535;
+    Voltage2[ConversionCount] = (AdcRegs.ADCRESULT1 >> 4) -1535;
+
+    //
+    // If 40 conversions have been logged, start over
+    //
+    if(ConversionCount == 1500)
+    {
+        ConversionCount = 0;
+    }
+    else
+    {
+        ConversionCount++;
+    }
+
+    //
+    // Reinitialize for next ADC sequence
+    //
+    AdcRegs.ADCTRL2.bit.RST_SEQ1 = 1;         // Reset SEQ1
+    AdcRegs.ADCST.bit.INT_SEQ1_CLR = 1;       // Clear INT SEQ1 bit
+
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;   // Acknowledge interrupt to PIE
+
+    return;
+}
+
+//
+// End of File
+//
+
+
